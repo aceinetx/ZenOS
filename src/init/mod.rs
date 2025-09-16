8a0000b4155eac13f71c73b4d54a6faccc36ab6f
aceinetx <mouselin82@gmail.com>
2025-09-16 21:29:30 +0300

    Update

--- src/init/mod.rs
+++ src/init/mod.rs
@@ -5,21 +5,27 @@ use uefi_services::*;
 pub fn zen_main() -> Result<(), &'static str> {
     println!();
 
-    let code = String::from("fn main { return 123; }");
-    let mut tokenizer = tokenizer::Tokenizer::new(code);
-    let mut compiler = compiler::Compiler::new(&mut tokenizer);
-    if let Err(e) = compiler.compile() {
-        println!("compilation error: {}", e);
+    {
+        let code = String::from("fn main { return 123; }");
+        let mut tokenizer = tokenizer::Tokenizer::new(code);
+        let mut compiler = compiler::Compiler::new(&mut tokenizer);
+        if let Err(e) = compiler.compile() {
+            println!("compilation error: {}", e);
+        }
+
+        let bytes = compiler.get_bytes();
+        for i in 0..bytes.len() {
+            let byte = bytes[i];
+            print!("{:x}     ", byte);
+            if byte != 0 {
+                print!("{}", byte as char);
+            }
+            println!();
+        }
     }
 
-    let bytes = compiler.get_bytes();
-    for i in 0..bytes.len() {
-        let byte = bytes[i];
-        print!("{:x}     ", byte);
-        if byte != 0 {
-            print!("{}", byte as char);
-        }
-        println!();
+    {
+        let module = vm::Module::new();
     }
 
     Ok(())
--- src/lang/vm.rs
+++ src/lang/vm.rs
@@ -1,15 +1,37 @@
 use alloc::string::String;
 use alloc::vec::Vec;
 
-pub enum Opcode {
-    Return(String),
-}
+pub enum Opcode {}
 
 pub struct Function {
     name: String,
     code: Vec<Opcode>,
 }
 
-pub struct FileFormat {
+pub struct Module {
     functions: Vec<Function>,
+}
+
+impl Module {
+    pub fn new() -> Module {
+        return Module {
+            functions: Vec::new(),
+        };
+    }
+}
+
+pub struct VM<'a> {
+    stack: Vec<(&'a mut Function, usize)>,
+    modules: Vec<&'a mut Module>,
+}
+
+impl<'a> VM<'_> {
+    pub fn new() -> VM<'a> {
+        return VM {
+            stack: Vec::new(),
+            modules: Vec::new(),
+        };
+    }
+
+    pub fn load_module(module: Module) {}
 }
