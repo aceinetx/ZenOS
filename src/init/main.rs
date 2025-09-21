7b17cdaab68b8ea9ec4b81a441f5367eeddea583
aceinetx <mouselin82@gmail.com>
2025-09-21 12:43:16 +0300

    remove unnecessary code

--- src/init/main.rs
+++ src/init/main.rs
@@ -1,11 +1,6 @@
 use crate::lang::kernel_interpreter::kernel_interpreter;
-use crate::lang::*;
-use alloc::string::String;
-use uefi::println;
 
 pub fn main() -> Result<(), &'static str> {
-    println!();
-
     kernel_interpreter();
 
     Ok(())
